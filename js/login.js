2f716ea68acecfdc65126975da779d1d981ee636
Anto-dot <antonellaromero192@gmail.com>
2026-06-11 11:24:29 -0300

    Add login form submission handler

---
+++ js/login.js
@@ -0,0 +1,9 @@
+const formulario = document.getElementById("formLogin");
+
+formulario.addEventListener("submit", function(event){
+
+event.preventDefault();
+
+window.location.href="index.html";
+
+});
